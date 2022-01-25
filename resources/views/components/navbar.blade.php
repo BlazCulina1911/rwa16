3a301017eb4d4106607bea263ca18f2822cfbd81
BlazCulina1911 <94997025+BlazCulina1911@users.noreply.github.com>
2022-01-25 05:11:08 +0100

    Fixed navbar
    Navbar now includes blade component {{ (request()->is('correct_link')) ? 'active' : '' }} to correctly show an active link :+1:

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,16 @@
 <div class="w3-top">
     <div class="w3-bar w3-red w3-card w3-left-align w3-large">
         <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-        <a href="{{ url('/') }}" class="w3-bar-item w3-button w3-padding-large w3-white">Početna</a>
-        <a href="{{ url('/contact') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Kontakt</a>
-        <a href="{{ url('/about') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">O nama</a>
+        <a href="{{ url('/') }}" class="w3-bar-item w3-button w3-padding-large {{ (request()->is('/')) ? 'w3-white' : ''}} ">Početna</a>
+        <a href="{{ url('/contact') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white {{ (request()->is('contact')) ? 'w3-white' : ''}} ">Kontakt</a>
+        <a href="{{ url('/about') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white {{ (request()->is('about')) ? 'w3-white' : ''}} ">O nama</a>
         @auth
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <button class="w3-bar-item w3-button w3-padding-large w3-right" type="submit">Odjavi se</button>
             </form>
-            <a href="{{ url('/reservations') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Rezervacija</a>
-            <a href="{{ url('/profile') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white w3-right">Moj Profil</a>
+            <a href="{{ url('/reservations') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white {{ (request()->is('reservations')) ? 'w3-white' : ''}}">Rezervacija</a>
+            <a href="{{ url('/profile') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white w3-right {{ (request()->is('profile')) ? 'w3-white' : ''}}">Moj Profil</a>
         @endauth
         @guest
             <a href="{{ url('register') }}" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white w3-right">Registracija</a>
